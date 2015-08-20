8e953aed5e1b1326c043312ad157406c917fd46b
Elkin <elkin@iMac-de-Nicolas.local>
2015-08-19 20:06:41 -0500

    Geolacalizacion del usuario

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('tuCocina', ['ionic', 'tuCocina.controllers', 'tuCocina.services'])
+angular.module('tuCocina', ['ionic', 'ngCordova', 'tuCocina.controllers', 'tuCocina.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -25,11 +25,19 @@ angular.module('tuCocina', ['ionic', 'tuCocina.controllers', 'tuCocina.services'
       templateUrl: 'partials/home.html',
       controller: 'homeController'
     })
+    
     $stateProvider
     .state('direccion', {
       url: '/direccion',
-      templateUrl: 'partials/direccion.html'
-      // controller: 'homeController'
+      templateUrl: 'partials/direccion.html',
+      controller: 'direccionController'
+    }) 
+
+    $stateProvider
+    .state('restaurantes', {
+      url: '/restaurantes',
+      templateUrl: 'partials/restaurantes.html',
+      controller: 'restaurantesController'
     });
 
     $urlRouterProvider.otherwise('/home');
